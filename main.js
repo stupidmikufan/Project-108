f38e3874db14430424a34399881daba402ba3a71
stupidmikufan <97323959+stupidmikufan@users.noreply.github.com>
2022-08-03 20:47:51 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,75 @@
+function startClassification()
+{
+    navigator.mediaDevices.getUserMedia({audio: true});
+    classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/BARXrH43i/model.json', modelReady);
+}
+
+function modelReady()
+{
+    classifier.classify(gotResults);
+}
+var lion = 0;
+var cat = 0;
+var dog = 0;
+
+
+function gotResults(error, results)
+{
+    if (error){
+        console.error(error);
+    }
+
+    else {
+        console.log(results);
+        random_number_r = Math.floor(Math.random() * 255) + 1;
+        random_number_g = Math.floor(Math.random() * 255) + 1;
+        random_number_b = Math.floor(Math.random() * 255) + 1;
+
+        document.getElementById("result_label").innerHTML = 'Detected voice is of - '+ results[0].label;
+        document.getElementById("result_count").innerHTML = 'Detected Dog - '+dog+ ' Detected Cat - '+cat+ 'Detected Lion'+lion;
+        document.getElementById("result_label").style.color = "rgb("+ random_number_r + "," + random_number_g + "," + random_number_b + ")";
+        document.getElementById("result_count").style.color = "rgb("+ random_number_r + "," + random_number_g + "," + random_number_b + ")";
+
+       
+        img = document.getElementById("listening");
+
+        if (results[0].label == "Bark"){
+            img.src = 'bark.gif';
+            dog = dog + 1;
+           
+        }
+
+        else if (results[0].label == "Purring"){
+            
+            img.src = 'purring.gif';
+            cat = cat + 1;
+            
+        }
+
+        else if (results[0].label == "Roar"){
+           
+            img.src = 'roar.gif';
+            lion = lion + 1;
+           
+        }
+
+       
+
+        else{
+           
+            img.src = 'listen.gif';
+        }
+
+
+        
+
+
+    }
+
+
+
+    
+
+}
+    
+
